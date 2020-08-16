9cb1d9c3bf510eccb717f7ee29aa907e84e6bbb3
Yurisn0w <yuri.yuki.6@gmail.com>
2020-08-16 14:45:22 +0900

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'static_pages#home'
 
   get  '/help',    to: 'static_pages#help'
 
   get  '/signup',  to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/signup',  to: 'users#create'
+  
+  resources :users
 end
